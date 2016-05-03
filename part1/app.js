b2381caf5453b83a50ff4889dcdc99000ccc6fee
Pankil <pandyapankil022@gmail.com>
2016-05-02 22:07:03 -0700

    part1

---
+++ part1/app.js
@@ -0,0 +1,39 @@
+ko.bindingHandlers.fadeText = {
+    update: function(element, valueAccessor) {
+        $(element).hide();
+        ko.bindingHandlers.text.update(element, valueAccessor);
+        $(element).fadeIn();
+    }        
+};
+function AppViewModel(){		
+		var self = this;    
+		self.input = ko.observable("");           
+    self.comment = ko.observable("");
+    self.comments = ko.observableArray([
+    	{comment: 'This is the first comment!'},
+      {comment: 'Here\'s the second one!'},
+      {comment: 'And this is one more.'},
+      {comment: 'Here is another one!'}
+    ]);
+    self.check = function(data,event){
+    	try{
+      	if(event.which == 13){        
+        	if (self.input().trim() != ""){          
+          	var comment = {comment: self.input()};
+          	self.comments.push(comment);	
+          }   
+          return false;
+        }
+        return true;
+      }catch(e){}      
+    };
+    self.addComment = function(){      
+        	if (self.input().trim() != ""){          
+          	var comment = {comment: self.input()};
+          	self.comments.push(comment);	
+          }          
+    };  
+    
+}
+
+ko.applyBindings(new AppViewModel());
