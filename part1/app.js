e9960662f01ddbb2537acb29b3383c462496de11
Pankil <pandyapankil022@gmail.com>
2016-05-02 23:40:33 -0700

    app

--- part1/app.js
+++ part1/app.js
@@ -3,37 +3,47 @@ ko.bindingHandlers.fadeText = {
         $(element).hide();
         ko.bindingHandlers.text.update(element, valueAccessor);
         $(element).fadeIn();
-    }        
+    }
 };
-function AppViewModel(){		
-		var self = this;    
-		self.input = ko.observable("");           
+
+function AppViewModel() {
+    var self = this;
+    self.input = ko.observable("");
     self.comment = ko.observable("");
-    self.comments = ko.observableArray([
-    	{comment: 'This is the first comment!'},
-      {comment: 'Here\'s the second one!'},
-      {comment: 'And this is one more.'},
-      {comment: 'Here is another one!'}
-    ]);
-    self.check = function(data,event){
-    	try{
-      	if(event.which == 13){        
-        	if (self.input().trim() != ""){          
-          	var comment = {comment: self.input()};
-          	self.comments.push(comment);	
-          }   
-          return false;
-        }
-        return true;
-      }catch(e){}      
+    self.comments = ko.observableArray([{
+        comment: 'This is the first comment!'
+    }, {
+        comment: 'Here\'s the second one!'
+    }, {
+        comment: 'And this is one more.'
+    }, {
+        comment: 'Here is another one!'
+    }]);
+    self.check = function(data, event) {
+        try {
+            if (event.which == 13) {
+                if (self.input().trim() != '') {
+                    var comment = {
+                        comment: self.input()
+                    };
+                    self.comments.push(comment);
+                    self.input('');
+                }
+                return false;
+            }
+            return true;
+        } catch (e) {}
     };
-    self.addComment = function(){      
-        	if (self.input().trim() != ""){          
-          	var comment = {comment: self.input()};
-          	self.comments.push(comment);	
-          }          
-    };  
-    
+    self.addComment = function() {
+        if (self.input().trim() != '') {
+            var comment = {
+                comment: self.input()
+            };
+            self.comments.push(comment);
+            self.input('');
+        }
+    };
+
 }
 
 ko.applyBindings(new AppViewModel());
